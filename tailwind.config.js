a229ee4161a0fc6ff8929b1c9dcf0da5dfc8ccf9
じぇいど <83527821+jade-banares@users.noreply.github.com>
2023-09-17 16:28:37 +0800

    done with the challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "976px",
+      },
+      colors: {
+        violet: "hsl(257, 40%, 49%)",
+        softMagenta: "hsl(300, 69%, 71%)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        openSans: ["Open Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
